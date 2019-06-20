4d433a9af53ece7cf965ca11031e48d28bba4de9
Mike Zurzolo <mike@hsowen.com>
2019-06-19 22:00:46 -0400

    added blank project1.py.backup file

--- Python/Projects/project1/work_here.sh
+++ Python/Projects/project1/work_here.sh
@@ -3,9 +3,15 @@
 mkdir -p inputs outputs
 
 if [ ! -d ./py3 ] ; then
-python3 -m venv py3
-source py3/bin/activate
-python3 -m pip install -r requirements.txt
+  python3 -m venv py3
+  source py3/bin/activate
+  python3 -m pip install -r requirements.txt
+fi
+
+if [ ! -f ./project1.py ] ; then
+  cp ./project1.py.backup ./project1.py
 fi
 
 source py3/bin/activate
+
+echo "Remember to run \"deactivate\" when you're done with this project"
