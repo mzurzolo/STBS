ac5bc49516699a787092c21e9a3b8a377cfec998
Mike Zurzolo <mikezurzolo@gmail.com>
2019-11-19 16:51:54 -0500

    Made project5 a template project

---
+++ Python/Projects/project5/README.md
@@ -0,0 +1,25 @@
+## Project 5
+
+### If you haven't already, please read [project notes](../README.md).
+
+## Goal: Create your own project.
+
+In this example, I only include a template.
+
+## Project Setup:
+
+- Open a terminal window
+- Navigate to the STBS directory: `cd ~/STBS`
+- Get STBS Updates: `git pull`
+- Navigate to the project directory: `cd ~/STBS/Python/Projects/project5`
+- Run the Project Setup Script: `source work_here.sh`
+- Collect data
+- Export the spreadsheet as a csv file
+  - `File` -> `Download` -> `Comma-separated values (.csv)` for Google Sheets
+  - `File` -> `Save as...` -> `Text CSV (.csv)` for LibreOffice Calc
+
+## Files:
+
+- Extra project files can be downloaded from the Google Drive [shared folder](https://drive.google.com/drive/folders/19lm31blakFJes99gz_KqloNM8IEqbpLa?usp=sharing)
+
+#### [Back to Home](https://skiptheboringstuff.com)
---
+++ Python/Projects/project5/work_here.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+# work_here.sh is a shell script (it's not python).
+# shell scripts are just another way to tell your computer what to do.
+# you could perform all of the actions
+# in this script manually (on a command line), but these steps should not be
+# the focus of the lesson. I put the shell script here so you could ignore
+# the details of project setup for now. I'll write a translation/explanation
+# of what's happening right above each line.
+
+# "Make two directories (folders), named 'inputs' and 'outputs'"
+# The p flag (-p) means that it's ok if the folders exist already.
+# see note 1 below for more details.
+mkdir -p inputs outputs
+
+git pull
+
+read -p "Reset python environment? (y/n)" result
+case "$result" in
+  y|Y ) rm -fR py3 ;;
+  n|N ) true ;;
+esac
+
+# "If there is not a directory called py3, then do the following things:
+#    1. create a virtual environment here called py3
+#    2. activate that environment so all python commands run inside the new
+#        environment
+#    3. upgrade python's package installer (pip)
+#    4. install all the packages listed in requirements.txt"
+
+if [ ! -d ./py3 ] ; then
+  python3 -m venv ./py3
+  source ./py3/bin/activate
+  python3 -m pip install --upgrade pip
+  python3 -m pip install -r requirements.txt
+fi
+
+# "If there is not a file called project5.py, then
+# copy the file project5.backup.py and call the project5.py"
+# I copy projects this way so the project's "starting point" is saved,
+# and you can make changes to project5.py without worrying about losing the
+# original. It also allows you to get updated projects (with git pull) without
+# those updates overwriting your current work.
+if [ ! -f ./project5.py ] ; then
+  cp ./project5.backup.py ./project5.py
+fi
+
+# "activate the virtual environment"
+# This makes sure the virtual environment gets activated even if it already
+# exists.
+source ./py3/bin/activate
+
+# This starts jupyter lab in your browser. Starting jupyter lab from the script
+# Makes sure that when you stop jupyter lab, you don't need to deactivate your
+# environment
+jupyter lab
+
+# Note 1: The p flag also creates parent directories as needed, but that's not
+# a relevant detail for how I'm using it here. Parent directories are folders
+# that hold other folders. The command "mkdir -p parent1/child1/inputs" will
+# create: a folder parent1 (if it doesn't exist),
+#         a folder child1 inside of parent1 (if it doesn't exist),
+#         and a folder inputs inside of child1 (if it doesn't exist).
+# The command "mkdir parent1/child1/inputs" will only create the last folder
+# (inputs) and it will only do that if the other two folders already exist.
+# If they don't, or if the inputs folder already exists,
+# "mkdir parent1/child1/inputs" will produce an error message.
