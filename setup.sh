0e3b5fcb88f7d488029ba71012787f36a2d97c70
Mike Zurzolo <mikezurzolo@gmail.com>
2019-11-25 18:22:00 -0500

    remove anaconda installer after setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 ANACONDA_VERSION=Anaconda3-2019.03-Linux-x86_64.sh
-sudo apt-get update && sudo apt-get -y install libbz2-1.0:amd64 libgl1:amd64 libx11-dev:amd64
+sudo apt-get update && \
+sudo apt-get -y install libbz2-1.0:amd64 libgl1:amd64 libx11-dev:amd64
 
 wget https://repo.anaconda.com/archive/$ANACONDA_VERSION && \
-chmod +x $ANACONDA_VERSION &&\
-bash $ANACONDA_VERSION -b &&
-. ~/anaconda3/bin/activate && conda init && . ~/.bashrc
+chmod +x $ANACONDA_VERSION && \
+bash $ANACONDA_VERSION -b && \
+. ~/anaconda3/bin/activate && \
+conda init && \
+rm $ANACONDA_VERSION && \
+. ~/.bashrc
